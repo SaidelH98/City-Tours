e9564e6529d2d445219f1649ac69c6505c5f2a7f
Saidel Halol <saidelhalol98@gmail.com>
2023-08-08 08:33:12 -0700

    Table updates

--- java/database/data.sql
+++ java/database/data.sql
@@ -5,5 +5,12 @@ INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpUL
 
 
 
-INSERT INTO landmarks (name, venue_type, day_of_operation, image, description) VALUES ('The Colosseum', 'Museum', 'Monday', 'https://images.unsplash.com/photo-1552832230-c0197dd311b5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y29sb3NzZXVtfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60', 'Packed with ancient history, the Colosseum is one of the most recognized landmarks and ruins in Rome. Dating back over 2,000 years it was once the ground for legendary gladiator contests that would entertain the masses.');
+INSERT INTO landmarks (name, city, country, venue_type, image, description, address) VALUES ('The Colosseum', 'Rome', 'Italy', 'Museum', 'https://images.unsplash.com/photo-1552832230-c0197dd311b5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y29sb3NzZXVtfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60', 'Packed with ancient history, the Colosseum is one of the most recognized landmarks and ruins in Rome. Dating back over 2,000 years it was once the ground for legendary gladiator contests that would entertain the masses.', 'Piazza del Colosseo, 1, 00184 Roma RM, Italy');
+INSERT INTO schedule (landmark_id, day_of_operation, open_time, close_time) VALUES (1, 'Sunday', '9:00', '19:00');
+INSERT INTO schedule (landmark_id, day_of_operation, open_time, close_time) VALUES (1, 'Monday', '9:00', '19:00');
+INSERT INTO schedule (landmark_id, day_of_operation, open_time, close_time) VALUES (1, 'Tuesday', '9:00', '19:00');
+INSERT INTO schedule (landmark_id, day_of_operation, open_time, close_time) VALUES (1, 'Wednesday', '9:00', '19:00');
+INSERT INTO schedule (landmark_id, day_of_operation, open_time, close_time) VALUES (1, 'Thursday', '9:00', '19:00');
+INSERT INTO schedule (landmark_id, day_of_operation, open_time, close_time) VALUES (1, 'Friday', '9:00', '19:00');
+INSERT INTO schedule (landmark_id, day_of_operation, open_time, close_time) VALUES (1, 'Saturday', '9:00', '19:00');
 COMMIT TRANSACTION;
--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS schedule, landmarks, users;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -13,11 +13,23 @@ CREATE TABLE users (
 CREATE TABLE landmarks (
 	landmark_id SERIAL,
 	name varchar(50) NOT NULL UNIQUE,
+	city varchar(50) NOT NULL,
+	country varchar(50) NOT NULL,
     venue_type varchar(200) NOT NULL,
-	day_of_operation varchar(50) NOT NULL,
 	image varchar(500) NOT NULL,
 	description varchar(2000) NOT NULL,
+	address varchar(500) NOT NULL,
 	CONSTRAINT PK_landmark PRIMARY KEY (landmark_id )
+);
+
+CREATE TABLE schedule (
+    id SERIAL,
+    landmark_id INT NOT NULL,
+    day_of_operation varchar(50) NOT NULL,
+    open_time varchar(50) NOT NULL,
+    close_time varchar(50) NOT NULL,
+    CONSTRAINT fk_landmark FOREIGN KEY(landmark_id) REFERENCES landmarks(landmark_id),
+    CONSTRAINT PK_schedule PRIMARY KEY (id)
 );
 
 COMMIT TRANSACTION;
