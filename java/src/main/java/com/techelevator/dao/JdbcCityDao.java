9a2bab53352d5806a26cf4d1b38d9baee560121e
Nick Ngouma <nickprincy@gmail.com>
2023-08-09 09:39:36 -0700

    Added Component

--- java/src/main/java/com/techelevator/dao/JdbcCityDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcCityDao.java
@@ -3,10 +3,12 @@ package com.techelevator.dao;
 import com.techelevator.model.City;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class JdbcCityDao implements CityDao{
 
     private final JdbcTemplate jdbcTemplate;
