58855a4470dc4074b4cb84caafbe2177e1a5dbab
Austin Christensen <austinchristensen09@gmail.com>
2023-08-11 08:02:52 -0700

    itinerary table

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS schedule, landmarks, city, users;
+DROP TABLE IF EXISTS itinerary_landmarks, itinerary, schedule, landmarks, city, users;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -39,6 +39,23 @@ CREATE TABLE schedule (
     close_time int NOT NULL,
     CONSTRAINT fk_landmark FOREIGN KEY(landmark_id) REFERENCES landmarks(landmark_id),
     CONSTRAINT PK_schedule PRIMARY KEY (id)
+);
+
+CREATE TABLE itinerary (
+    itinerary_id SERIAL,
+    name varchar(50) NOT NULL,
+    starting_point varchar(500) NOT NULL,
+    date date,
+    CONSTRAINT PK_itinerary PRIMARY KEY (itinerary_id)
+);
+
+CREATE TABLE itinerary_landmarks (
+    itinerary_id int NOT NULL,
+    landmark_id int NOT NULL,
+    itinerary_order int NOT NULL,
+    CONSTRAINT pk_itinerary_landmarks PRIMARY KEY (itinerary_id, landmark_id),
+    CONSTRAINT fk_itinerary_landmarks_landmarks FOREIGN KEY (landmark_id) REFERENCES landmarks(landmark_id),
+    CONSTRAINT fk_itinerary_landmarks_itinerary FOREIGN KEY (itinerary_id) REFERENCES itinerary(itinerary_id)
 );
 
 
